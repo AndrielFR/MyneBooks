1003fc89cd2df46e416fa05341b60d93c99d158a
AndrielFR <andrielkogama2@gmail.com>
2022-03-13 23:24:53 -0300

    myne: handlers: connect start to about
    - also change to our fork.

--- src/handlers/start.rs
+++ src/handlers/start.rs
@@ -1,28 +1,47 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2022 Andriel Ferreira <https://github.com/AndrielFR>
 
-use grammers_client::{InputMessage};
+use grammers_client::{InputMessage, types};
 
 use crate::utils;
 use crate::dyn_async;
 use crate::handler::{Data, HandlerOptions, Register};
+use crate::language::I18n;
 
 
 #[macro_rules_attribute(dyn_async!)]
 async fn start_message<'fut>(data: &'fut Data) {
+    let client = data.client;
     let message = data.message.unwrap();
     let lang = data.language;
     let me = data.me;
 
-    message
-        .reply(InputMessage::html(
-            lang.get_text("texts.start", vec![("bot_username", me.username().unwrap())]))
-            .reply_markup(&utils::make_keyboard(
-                 vec![vec![(&lang.get_text("buttons.about", vec![]), "about$")]]
-            ))
-        )
+    client
+        .send_message(&message.chat(), get_start_message(lang, me).reply_to(Some(message.id())))
         .await
         .expect("Failed to reply the message");
+}
+
+#[macro_rules_attribute(dyn_async!)]
+async fn start_callback<'fut>(data: &'fut Data) {
+    let client = data.client;
+    let callback = data.callback.unwrap();
+    let lang = data.language;
+    let me = data.me;
+
+    let message = callback.load_message().await.unwrap();
+
+    client
+        .edit_message(callback.chat(), message.id(), get_start_message(lang, me))
+        .await
+        .expect("Failed to answer the callback");
+}
+
+fn get_start_message(lang: I18n, me: &types::User) -> InputMessage {
+    InputMessage::html(lang.get_text("texts.start", vec![("bot_username", me.username().unwrap())]))
+        .reply_markup(&utils::make_keyboard(
+            vec![vec![(&lang.get_text("buttons.about", vec![]), "about")]]
+        ))
 }
 
 
@@ -31,5 +50,6 @@ pub fn initialize<'a>() -> Register<'a> {
         .set_name("start")
         .set_enabled(true)
         .append("message", start_message, "start$", Some(HandlerOptions { is_command: true, description: Some("Start the bot"), ..Default::default() }))
+        .append("callback", start_callback, "^start$", None)
         .build()
 }
